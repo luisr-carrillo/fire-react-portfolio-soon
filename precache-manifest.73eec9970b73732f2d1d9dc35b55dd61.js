cc1a38fee88e62e0e2720dbbea75e865bdf79945
Luis R. Carrillo <luisr_carrillo@outlook.com>
2020-03-13 21:43:19 -0600

    Updates

---
+++ precache-manifest.73eec9970b73732f2d1d9dc35b55dd61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a9269c8e1199aeeb8473767710c04f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "842b530c8ef02f6b6f86",
+    "url": "/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "52865a605e69b4dd5f26",
+    "url": "/static/css/main.88f5a213.chunk.css"
+  },
+  {
+    "revision": "842b530c8ef02f6b6f86",
+    "url": "/static/js/2.0922e689.chunk.js"
+  },
+  {
+    "revision": "d097a9aa70e716a11d3e554e80c108fd",
+    "url": "/static/js/2.0922e689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52865a605e69b4dd5f26",
+    "url": "/static/js/main.b7e3c811.chunk.js"
+  },
+  {
+    "revision": "ef8138f66d9656828199",
+    "url": "/static/js/runtime-main.b5b7ad67.js"
+  },
+  {
+    "revision": "5467f594a51daeebd4b66d015686285d",
+    "url": "/static/media/bricks-background.5467f594.jpeg"
+  }
+]);
